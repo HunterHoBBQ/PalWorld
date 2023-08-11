860da9efd319bdc40e00781a19fcbf289f2face8
HunterHoBBQ <HunterHoBBQ@gmail.com>
2023-08-11 18:39:12 +0800

    v1.0.3 fixing the github page

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
-
+export default {
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-github-pages" : "",
-    },
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
   },
 };
-
-export default config;
